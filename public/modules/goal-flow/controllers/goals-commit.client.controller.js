1e0b9f3c79d4c0c19686f0980e1ebb39470e155a
Pim Bellinga <pimbellinga@gmail.com>
2015-02-17 16:56:51 +0100

    adds some styling and changes spider state to visualise

--- public/modules/goal-flow/controllers/goals-commit.client.controller.js
+++ public/modules/goal-flow/controllers/goals-commit.client.controller.js
@@ -2,11 +2,13 @@
 
 angular.module('goal-flow').controller('GoalsCommitController', [
 	'$scope',
+	'$state',
 	'Mygoals',
-	function($scope, Mygoals) {
+	function($scope, $state, Mygoals) {
 		$scope.goals = [{name:'ik wil nu stoppen'},{name:'ik wil heel goed zijn'},{name:'ik wil NU broodjes'}];
 		$scope.mygoals = Mygoals;
 		$scope.keyword = undefined;
+		//$scope.error = '';
 
 		$scope.commit = function(index) {
 			var goal = $scope.goals.splice(index,1)[0]
@@ -17,6 +19,12 @@ angular.module('goal-flow').controller('GoalsCommitController', [
 		$scope.remove = function(index) {
 			var goal = $scope.mygoals.splice(index,1)[0]
 			$scope.goals.push(goal);
+		}
+
+		$scope.proceed = function() {
+			if (!$scope.mygoals.length) return $scope.error = 'please select at least one goal'
+
+			$state.go('rate');
 		}
 
 	}
